be47a3d022d06eec2e243312de8ab7309fb39759
Rin NoName <noname@MacBook-Pro-Rin.local>
2021-01-02 16:30:46 +0300

    new commit

--- auto/settings.py
+++ auto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v2^lgy3wg&p@%%(i9aclf!%ho)i1y_27-xbwi7xv6fmiy)^bij'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
